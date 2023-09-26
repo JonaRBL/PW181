f1a97d37d165fa1cefd3c1d2b273825704af9e81
JonaRBL <121039246@upq.edu.mx>
2023-09-25 23:33:00 -0600

    instalacion de las dependencias bootstrap, composer y edicion de codigo index y vreacion de rutas

--- Pract3/routes/web.php
+++ Pract3/routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/form', function () {
+    return view('formulario');
+});
+Route::get('/recuerdos', function () {
+    return view('recuerdos');
+});
+
+// Route::view('/', 'welcome');
+// Route::view('/form', 'formulario');
+// Route::view('/recuerdos', 'recuerdos');
+
